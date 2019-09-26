5f5ac94967cf1fe01e1a9bb450486d888a1244f5
Ankit Thakur <tankit541@gmail.com>
2019-09-26 12:44:12 +0530

    Added RadixSort file

---
+++ src/main/java/sorting/linearSorting/RadixSort.java
@@ -0,0 +1,59 @@
+package main.java.sorting.linearSorting;
+
+import java.util.Arrays;
+
+public class RadixSort {
+
+    public int[] radixSort(int arr[]) {
+        int maxValue = maxValue(arr);
+        for (int i = 1; maxValue / i > 0; i *= 10) {
+            arr = sortArray(arr, i);
+        }
+        return arr;
+    }
+
+    private int[] sortArray(int[] arr, int digit) {
+
+        int arrayLength = arr.length;
+        int sortedArray[] = new int[arrayLength];
+
+        int count[] = new int[10];
+        for (int i = 0; i < count.length; i++) {
+            count[i] = 0;
+        }
+        for (int i = 0; i < arrayLength; i++) {
+            count[(arr[i] / digit) % 10]++;
+        }
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+        for (int i = arrayLength - 1; i >= 0; i--) {
+            sortedArray[count[(arr[i] / digit) % 10] - 1] = arr[i];
+            count[(arr[i] / digit) % 10]--;
+        }
+        for (int i = 0; i < arrayLength; i++) {
+            arr[i] = sortedArray[i];
+        }
+
+        return arr;
+    }
+
+    static int maxValue(int arr[]) {
+        int maxValue = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] > maxValue)
+                maxValue = arr[i];
+        return maxValue;
+    }
+
+    public static void main(String[] args) {
+        RadixSort obj = new RadixSort();
+        int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
+
+        System.out.println("Array Before Sorting : ");
+        System.out.println(Arrays.toString(arr));
+        obj.radixSort(arr);
+        System.out.println("Array After Sorting : ");
+        System.out.println(Arrays.toString(arr));
+    }
+}
